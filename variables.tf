9b4ee2ddab731604848089d5970b33ccdbc11aaa
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-03-09 22:27:23 +0530

    vpc module test

--- variables.tf
+++ variables.tf
@@ -7,25 +7,25 @@ variable "environment" {
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+    default = "10.0.0.0/16"
 }
 
 variable "common_tags" {
     default = {
-        poject = "expense"
+        Project = "expense"
         Terraform = "true"
         Environment = "dev"
     }
 }
 
-variable "public_sebnet_cidrs" {
+variable "public_subnet_cidrs" {
     default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_sebnet_cidrs" {
+variable "private_subnet_cidrs" {
     default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
-variable "database_sebnet_cidrs" {
+variable "database_subnet_cidrs" {
     default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,11 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/daws-81s/terraform-aws-vpc.git?ref=main"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
-    public_subnet_cidrs = var.public_sebnet_cidrs
-    private_subnet_cidrs = var.private_sebnet_cidrs
-    database_subnet_cidrs = var.database_sebnet_cidrs
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
 }
